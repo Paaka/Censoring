481f68ef3db284bfbe3243461197a25b0cd6ecc8
Hartabus Mateusz <mateusz.hartabus@microsoft.wsei.edu.pl>
2019-05-22 14:37:45 +0200

    Added Aplication

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var songAnalysys = new songAnalysys(band:"kazik", song: "12 groszy");
+            Console.ReadLine();
+        }
+    }
+
+    class songAnalysys
+    {
+        public songAnalysys(string band, string song)
+        {
+            var browser = new WebClient();
+            var url = "https://api.lyrics.ovh/v1/" + band + "/" + song;
+            var json = browser.DownloadString(url);
+            Console.WriteLine(json);
+           
+        }
+    }
+}
