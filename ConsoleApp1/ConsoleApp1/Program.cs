7d8de922abc6a94103cc8505cb3c46a1af3251d5
Paaka <MateuszHartabus@gmail.com>
2019-05-24 15:00:36 +0200

    C# 24.05.2019

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -13,18 +14,45 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
+            var tekst = "Programing is fucking awesome";
             var songAnalysys = new songAnalysys(band:"kazik", song: "12 groszy");
             var censor = new Censor();
+            Console.WriteLine(censor.Fix(tekst));
             Console.ReadLine();
+            
         }
     }
 
-    internal class Censor
+     class Censor
     {
+        string[] badWords;
         public Censor()
         {
             var profanities = File.ReadAllText("profanities.txt");
-     
+            profanities = profanities.Replace("*", "");
+            profanities = profanities.Replace("(", "");
+            profanities = profanities.Replace(")", "");
+            profanities = profanities.Replace("\"", "");
+
+            badWords = profanities.Split(',');
+
+            
+        }
+
+        public string Fix(string tekst)
+        {
+            foreach (var word in badWords)
+            {
+                tekst = RepalaceBadWords(tekst, word);
+            }
+
+            return tekst;
+        }
+
+        private static string RepalaceBadWords(string tekst, string word)
+        {
+            var pattern = "\\b" + word + "\\b";
+             return Regex.Replace(tekst, pattern, "_____", RegexOptions.IgnoreCase);
         }
     }
 
@@ -37,6 +65,7 @@ namespace ConsoleApp1
             var json = browser.DownloadString(url);
             var lyrics = JsonConvert.DeserializeObject<LyricovhAnwser>(json);
             Console.WriteLine(lyrics.lyrics);
+
            
         }
     }
